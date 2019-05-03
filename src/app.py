fa759f5ad4708a5805f0aa8783e9a5f9f0a89448
Jonathon Pemberton <jonpemby@icloud.com>
2019-05-03 10:40:02 -0500

    Put main code inside of app function

--- src/app.py
+++ src/app.py
@@ -4,30 +4,31 @@ from src.exceptions import ResponseError
 from src.utilities import print_err
 
 
-(queries, options) = read_args()
+def app():
+    (queries, options) = read_args()
 
-results = []
+    results = []
 
-# TODO:
-# 1) Make this actually multi-threaded using the multi-threading module... will reduce time to search by a lot!
-# 2) Insert results into the database once they are all processed and
-#    found (if the particular job isn't already in the db, that is)
+    # TODO:
+    # 1) Make this actually multi-threaded using the multi-threading module... will reduce time to search by a lot!
+    # 2) Insert results into the database once they are all processed and
+    #    found (if the particular job isn't already in the db, that is)
 
-for query in queries:
-    print("Searching for {}".format(query))
-    searcher = Searcher(query, options)
+    for query in queries:
+        print("Searching for {}".format(query))
+        searcher = Searcher(query, options)
 
-    try:
-        for result in searcher.run():
-            results.append(result)
-    except ResponseError as e:
-        print_err("There was an error while searching:\n"
-                  "{}".format(e))
-        break
+        try:
+            for result in searcher.run():
+                results.append(result)
+        except ResponseError as e:
+            print_err("There was an error while searching:\n"
+                      "{}".format(e))
+            break
 
-    print("Done searching for {}".format(query))
+        print("Done searching for {}".format(query))
 
-for result in results:
-    print(result)
+    for result in results:
+        print(result)
 
-exit(0)
+    exit(0)
